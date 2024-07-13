5a1e7536d2078602428d8db00e65765695e5bc47
shaansuraj <surajsahu658@gmail.com>
2024-07-13 23:33:20 +0530

    Corrected file issue in production

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
     port: process.env.PORT || 3000,
-    filesDir: path.join(__dirname, '..', 'files'),
+    filesDir: path.join(__dirname, '..', '/files'),
     deletedFilesDir: path.join(__dirname, '../deleted')
 };
